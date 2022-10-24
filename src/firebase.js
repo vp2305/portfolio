1b7540059f5d30a32ec59ad3a0c17a720a38b0f3
Vaibhav Patel <pate9020@mylaurier.ca>
2022-10-24 18:46:48 -0400

    Updated to have firebase v9

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,16 @@
 import firebase from "firebase";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-const firebaseApp = firebase.initializeApp({
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
     apiKey: "AIzaSyDsgQmlsUO9fMGUG89JfTj2aTCtYNjvOuA",
     authDomain: "vaibhav-portfolio.firebaseapp.com",
     projectId: "vaibhav-portfolio",
@@ -9,11 +18,14 @@ const firebaseApp = firebase.initializeApp({
     messagingSenderId: "28268301920",
     appId: "1:28268301920:web:fbc6f630b473c18be97d05",
     measurementId: "G-0GQW587Q18"
-});
+};
 
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-const storage = firebase.storage(); //this will be used to upload the pictures
-const googleAnalytics = getAnalytics(firebaseApp);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-export { db, auth, storage, googleAnalytics };
+const db = getFirestore();
+const auth = getAuth(app);
+const storage = getStorage(app); //this will be used to upload the pictures
+
+export { db, auth, storage, analytics };
